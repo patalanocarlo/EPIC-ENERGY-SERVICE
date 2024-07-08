898df0a5fd9268879756e8e3b0d3b13cada5809e
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-07-08 10:39:49 +0200

    Initial commit

---
+++ src/main/java/BuildWeekU5/EPIC/ENERGY/SERVICE/EpicEnergyServiceApplication.java
@@ -0,0 +1,13 @@
+package BuildWeekU5.EPIC.ENERGY.SERVICE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BuildWeekU5/EPIC/ENERGY/SERVICE/EpicEnergyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package BuildWeekU5.EPIC.ENERGY.SERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
