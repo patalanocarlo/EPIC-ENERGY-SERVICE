5c7bbd09e856518c1058766f1c657aca3ca4f780
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-07-09 09:14:28 +0200

    PayLoad Utente e aggiunta alla fatturaPayload

--- src/main/java/BuildWeekU5/EPIC/ENERGY/SERVICE/payloads/FatturePayload.java
+++ src/main/java/BuildWeekU5/EPIC/ENERGY/SERVICE/payloads/FatturePayload.java
@@ -1,5 +1,6 @@
 package BuildWeekU5.EPIC.ENERGY.SERVICE.payloads;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,6 +10,7 @@ public record FatturePayload(
         @NotNull(message = "Campo obbligatorio")
          LocalDate DataFattura,
        @NotNull(message = "Campo obbligatario")
+       @Min(value = 0 , message = "L'Importo deve essere maggiore di zero per essere valido ")
        double Importo,
         @NotEmpty(message = "Campo obbligatorio")
         String statoFattura
